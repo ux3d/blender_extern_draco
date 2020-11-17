bbfa297bbd8ade3025a2fbbc83397769cb248601
Jim Eckerlein <eckerlein@ux3d.io>
2020-11-17 11:48:39 +0100

    Stub functions for decoding

--- src/decoder.h
+++ src/decoder.h
@@ -1,3 +1,27 @@
 #pragma once
 
 #include "auxiliary.h"
+
+/**
+ * The opaque Draco encoder.
+ * A single instance is only intended to encode a single primitive.
+ */
+struct DracoDecoder;
+
+DLL_EXPORT(DracoDecoder *)
+decoderCreate ();
+
+DLL_EXPORT(void)
+decoderDecoder (DracoDecoder const *decoder);
+
+DLL_EXPORT(uint64_t)
+decoderByteLength (DracoDecoder const *decoder);
+
+DLL_EXPORT(void)
+encoderCopy (
+    DracoDecoder const *decoder,
+    uint8_t *o_data
+);
+
+DLL_EXPORT(void)
+decoderRelease (DracoDecoder const *decoder);
