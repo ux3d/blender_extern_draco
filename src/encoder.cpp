23cecec95bc6b71817cc4de9b1b835ed71c41b48
Jim Eckerlein <eckerlein@ux3d.io>
2020-11-24 15:46:08 +0100

    Add Apache-2.0 license notice into each source file

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @author Jim Eckerlein <eckerlein@ux3d.io>
  * @date   2019-11-18
@@ -199,12 +213,6 @@ API(uint32_t) encoderSetAttribute(Encoder *encoder, char *attributeName, size_t 
     size_t componentCount = getNumberOfComponents(dataType);
     size_t stride = getAttributeStride(componentType, dataType);
     draco::DataType dracoDataType = getDataType(componentType);
-    
-    if (dracoDataType == draco::DataType::DT_INVALID)
-    {
-        printf(LOG_PREFIX "Unsupported component type %zu.\n", componentType);
-        return -1;
-    }
     
     draco::GeometryAttribute::Type semantics = getAttributeSemantics(attributeName);
     draco::GeometryAttribute attribute;
